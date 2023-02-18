171f198cb5e8e10ca59914882593ef56eb46b157
sharlearn <liewsharlen@gmail.com>
2023-02-18 08:34:51 +0800

    Added base Auth0 setup, currently only retrieves token and user info.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [accessToken, setAccessToken] = useState("");
+
+  const { isLoading, isAuthenticated, getAccessTokenSilently, user } =
+    useAuth0();
+
+  const checkUser = async () => {
+    if (isAuthenticated) {
+      let token = getAccessTokenSilently();
+      setAccessToken(token);
+      console.log(accessToken);
+      console.log(user);
+    }
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, [isLoading]);
+
   return (
     <div className="App">
       <Header />
