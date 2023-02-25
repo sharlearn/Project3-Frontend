f05cf178aa57928a1816e645c01a29eaa55f62a3
nicolalew <nicola.lew@gmail.com>
2023-02-25 09:05:57 +0800

    Added filter by artist feature

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,6 +16,13 @@ const themes = [
   "Ocean",
 ];
 
+// const artists = [
+//   "johnt",
+//   "nicholasl",
+//   "gregorys",
+//   "nicolal",
+// ];
+
 const NavBar = () => {
   return (
     <Nav className="justify-content-center" bg="light">
@@ -31,10 +38,24 @@ const NavBar = () => {
       </NavDropdown>
       <NavDropdown title="artists">
         <NavDropdown.Item>
-          <Link to={`shirts`}>someone</Link>
+          <Link to={`shirts/artist/c03ade38-288f-4559-9058-6c8e9fee0773`}>
+            nicholasl
+          </Link>
         </NavDropdown.Item>
         <NavDropdown.Item>
-          <Link to={`shirts`}>someoneelse</Link>
+          <Link to={`shirts/artist/e02624ca-837d-4708-9276-74d4158f632a`}>
+            johnt
+          </Link>
+        </NavDropdown.Item>
+        <NavDropdown.Item>
+          <Link to={`shirts/artist/4ad97cbf-251e-4986-95f8-50463f36cd32`}>
+            gregorys
+          </Link>
+        </NavDropdown.Item>
+        <NavDropdown.Item>
+          <Link to={`shirts/artist/5f3822fb-df88-4f08-960c-9d2912ca5608`}>
+            nicolal
+          </Link>
         </NavDropdown.Item>
       </NavDropdown>
     </Nav>
